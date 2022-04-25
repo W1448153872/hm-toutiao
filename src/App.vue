838fee9b49bc7a491eb0935bc1c5f0b427d7608f
刘晶晶 <2826232478@qq.com>
2022-04-25 16:51:03 +0800

    引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box"></div>
   </div>
 </template>
 <script>
 export default {
-  name:'App'
-}
+  name: "App",
+};
 </script>
 
 <style lang="less">
+.box {
+  background-color: pink;
+  width: 200px;
+  height: 200px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "@/styles/index.less"
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+
+// 加载动态设置REM基准值
+import 'amfe-flexible'
+
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
